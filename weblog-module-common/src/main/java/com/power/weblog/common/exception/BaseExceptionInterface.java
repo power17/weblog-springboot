fd4da2e6662ddcee1a102807a2660c9e85b2c110
power <1410837981@qq.com>
2025-06-28 20:03:02 +0800

    feat: 后端项目基础工程搭建完成

---
+++ weblog-module-common/src/main/java/com/power/weblog/common/exception/BaseExceptionInterface.java
@@ -0,0 +1,6 @@
+package com.power.weblog.common.exception;
+
+public interface BaseExceptionInterface {
+    String getErrorCode();
+    String getErrorMessage();
+}
--- weblog-web/src/main/java/com/power/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/power/weblog/web/controller/TestController.java
@@ -2,19 +2,43 @@ package com.power.weblog.web.controller;
 
 
 import com.power.weblog.common.aspect.ApiOperationLog;
+import com.power.weblog.common.enums.BizException;
+import com.power.weblog.common.enums.ResponseCodeEnum;
+import com.power.weblog.common.utils.JsonUtil;
+import com.power.weblog.common.utils.Response;
 import com.power.weblog.web.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.stream.Collectors;
+
 @RestController
 @Slf4j
+@Api( tags = "首页模块")
 public class TestController {
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
-        // 返参
-        return user;
+    @ApiOperation(value = "测试接口")
+    public Response  test(@RequestBody @Validated User user) {
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
+
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
+
+        return Response.success(user);
     }
 }
